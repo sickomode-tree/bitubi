67324fc84f40ac08cc7133d337cc1b4c7fe7c8ea
Konstantin Zhitkov <KZhitkov@get-bc.local>
2018-10-31 17:42:13 +0300

    create floating footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { Button, Header, Modal } from 'semantic-ui-react'
+import './FloatingButton.scss'
 
 const Footer = () => (
-  <Container style={{fontSize: 12}}>
-    BITUBI - это электронный каталог поставщиков по Санкт-Петербургу и ЛО, с помощью которого закупщики выбирают лучшие
-    предложения поставщиков, а поставщики находят новых клиентов и увеличивают продажи
-  </Container>
+  <Modal
+    size={'tiny'}
+    trigger={<Button className={'FloatingButton'} icon={'info'} size={'huge'} circular />}
+  >
+    <Modal.Header>О нас</Modal.Header>
+    <Modal.Content>
+      <p>
+        BITUBI - это электронный каталог поставщиков по Санкт-Петербургу и ЛО, с помощью которого закупщики выбирают
+        лучшие предложения поставщиков, а поставщики находят новых клиентов и увеличивают продажи
+      </p>
+      <Header>Реквизиты</Header>
+      <p>
+        ООО "БИТУБИ", ИНН 7839106480, КПП 783901001, 190121, Санкт-Петербург г, Реки Пряжки наб, дом № 3/1,
+        литера А, квартира Пом/Ком 3-Н/332
+      </p>
+    </Modal.Content>
+  </Modal>
 )
 
 export default Footer
